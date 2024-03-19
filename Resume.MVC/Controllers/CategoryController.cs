5a628dfd2b04199eb5ce69158ad771302f21a6b2
pxr <pxr@DESKTOP-BIP5GPD>
2024-03-19 22:25:57 +0300

    category controller and views complete. some changes for other pages.

--- Resume.MVC/Controllers/CategoryController.cs
+++ Resume.MVC/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Resume.MVC.Models;
+using Resume.MVC.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,47 @@ namespace Resume.MVC.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryRepository categoryRepository = new CategoryRepository();
         public ActionResult Index()
         {
+            var values = categoryRepository.GetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(TblCategory tblCategory)
+        {
+            categoryRepository.TAdd(tblCategory);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            TblCategory t = categoryRepository.Find(x => x.CategoryId == id);
+            categoryRepository.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            TblCategory t = categoryRepository.Find(x => x.CategoryId == id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult EditCategory(TblCategory p)
+        {
+            TblCategory t = categoryRepository.Find(x => x.CategoryId == p.CategoryId);
+            t.CategoryName = p.CategoryName;
+            categoryRepository.TUpdate(t);
+            return RedirectToAction("Index");
+
         }
     }
 }
