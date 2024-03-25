ce9e095e101bcb35056d85c9fdc8267df4f53522
pxr <pxr@DESKTOP-BIP5GPD>
2024-03-25 23:25:58 +0300

    some changes

---
+++ Resume.MVC/Controllers/DefaultLayoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.MVC.Controllers
+{
+    public class DefaultLayoutController : Controller
+    {
+        // GET: DefaultLayout
+        public PartialViewResult DefaultLayoutHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult DefaultLayoutNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult DefaultLayoutScript()
+        {
+            return PartialView();
+        }
+    }
+}
